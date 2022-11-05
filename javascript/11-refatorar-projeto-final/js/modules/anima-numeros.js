5f94cc18ad846332a10da0c6ea1e1abc5b67666f
Paulo Henrique <58124197+Paulo-Henrique-Santana@users.noreply.github.com>
2022-11-05 03:01:26 -0300

    Adiciona o módulo Refatorar Projeto Final do curso de JS

---
+++ javascript/11-refatorar-projeto-final/js/modules/anima-numeros.js
@@ -0,0 +1,54 @@
+export default class AnimaNumeros {
+  constructor(numeros, observeTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros);
+    this.observeTarget = document.querySelector(observeTarget);
+    this.observerClass = observerClass;
+
+    // bind o this do objeto ao callback da mutação
+    this.handleMutation = this.handleMutation.bind(this);
+  }
+
+  // Recebe um elemento do dom, com número em seu texto
+  // incrementa a partir de 0 até o número final
+  static incrementarNumero(numero) {
+    const total = +numero.innerText;
+    const incremento = Math.floor(total / 100);
+    let start = 0;
+    const timer = setInterval(() => {
+      start += incremento;
+      numero.innerText = start;
+      if (start > total) {
+        numero.innerText = total;
+        clearInterval(timer);
+      }
+    }, 25 * Math.random());
+  }
+
+  // Ativa incrementar número para cada
+  // número selecionado do dom
+  animaNumeros() {
+    this.numeros.forEach((numero) => this.constructor.incrementarNumero(numero));
+  }
+
+  // Função que ocorre quando a mutação ocorrer
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect();
+      this.animaNumeros();
+    }
+  }
+
+  // Adiciona o MutationObserver para verificar
+  // quando a classe ativo é adicionada ao elemento target
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation);
+    this.observer.observe(this.observeTarget, { attributes: true });
+  }
+
+  init() {
+    if (this.numeros.length && this.observeTarget) {
+      this.addMutationObserver();
+    }
+    return this;
+  }
+}
---
+++ javascript/11-refatorar-projeto-final/js/modules/scroll-anima.js
@@ -0,0 +1,47 @@
+import debounce from './debounce.js';
+
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.checkDistance = debounce(this.checkDistance.bind(this), 200);
+  }
+
+  // Pega a distância de cada item em relação
+  // ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
+    });
+  }
+
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((section) => {
+      if (window.scrollY > section.offset) {
+        section.element.classList.add('ativo');
+      } else if (section.element.classList.contains('ativo')) {
+        section.element.classList.remove('ativo');
+      }
+    });
+  }
+
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
+  }
+}
